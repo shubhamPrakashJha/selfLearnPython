041250d2bbf16058921f7e6f2ae3874b91491f3a
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-08 17:57:42 +0530

    adding argument

--- function and module/funcArgument.py
+++ function and module/funcArgument.py
@@ -1,4 +1,14 @@
 def print_with_exclaimation(word):
     print(word+"!")
-print_with_exclaimation("ohhhk")
+
+def print_sum_twice(x,y):
+    print(x+y)
+    print(x+y)
+def function(variable):
+    variable += 1
+    print(variable)
+
+
 print_with_exclaimation("hello")
+print_sum_twice(2,3)
+function(7)
