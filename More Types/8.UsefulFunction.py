008c5b1fe1873c20fda6485a1e71c963f3700d4d
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-09 09:50:01 +0530

    new way to append list

--- More Types/8.UsefulFunction.py
+++ More Types/8.UsefulFunction.py
@@ -11,3 +11,11 @@ print(max(1,2,3,4,5,5,6,7,8))
 print(abs(-23))
 print(abs(23))
 print(sum([1,2,3,4,5,5,6,7,8]))
+print("------if--------------")
+nums = [55,44,33,22,11]
+if all(i>5 for i in nums):
+    print("all greater than 5")
+if any(i%2 ==0 for i in nums):
+    print("atleast one is even")
+for v in enumerate(nums):
+    print(v)
