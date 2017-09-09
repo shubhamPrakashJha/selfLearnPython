4942100f71fcc20c9df18916524b350eba1a59af
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-09 20:11:08 +0530

    add text analuzer

---
+++ More Types/9.TextAnalyzer.py
@@ -0,0 +1,15 @@
+def char_count(text,char):
+    count = 0
+    for c in text:
+        if c == char:
+            count += 1
+    return count
+
+
+filename = input("enter a filename:")
+with open(filename) as f:
+    text = f.read()
+
+for char in "Labcdefghijklmnopqrstuvwxyz":
+    perc = 100*char_count(text,char)/len(text)
+    print("{0} = {1}%".format(char,round(perc,2)))
