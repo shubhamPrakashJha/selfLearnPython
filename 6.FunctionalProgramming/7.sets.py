66ac5280fbc7b82648a4db9fc318b6e14942c2dd
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-10 18:20:06 +0530

    checking spam

--- 6.FunctionalProgramming/7.sets.py
+++ 6.FunctionalProgramming/7.sets.py
@@ -1,5 +1,17 @@
-num_set = {1,2,3,4,5}
+num_set = {1,2,3,4,5,6}
+add_set = {4,5,6,7,8,9}
 word_set = set(["spam","eggs","sausage"])
 
 print(3 in num_set)
-print("spam" not in word_set)
+print("sausage" not in word_set)
+#
+print(num_set | add_set)
+print(num_set & add_set)
+print(num_set - add_set)
+print(add_set - num_set)
+print(num_set ^ add_set)
+
+num_set.add(-5)
+print(num_set)
+num_set.remove(5)
+print(num_set)
