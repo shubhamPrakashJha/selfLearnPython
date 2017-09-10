e01890c0dd818d6970c34553009dc1a0035b2e9a
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-10 18:36:32 +0530

    adding itter tools

---
+++ 6.FunctionalProgramming/8.IterationTools.py
@@ -0,0 +1,14 @@
+from itertools import count,accumulate,takewhile,product,permutations
+
+for i in count(3):#counts from 3 to infinite
+    print(i)
+    if i>=11:
+        break
+
+nums = list(accumulate(range(8)))
+print(nums)
+print(list(takewhile(lambda x: x<=6,nums)))
+
+letters = ("A","B")
+print(list(product(letters,range(2))))
+print(list(permutations(letters)))
